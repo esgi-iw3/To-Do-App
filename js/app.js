74476481fd64acf278d07c0a55e7a6f517eb2d4e
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-05-15 00:28:00 +0200

    Suite

--- js/app.js
+++ js/app.js
@@ -1,12 +1,68 @@
 import { openDB } from '/node_modules/idb/build/esm/index.js';
-import CheckConnection from '/js/components/connection/connection.js';
+import checkConnectivity from '/js/connection.js';
+import MyTodo from "/js/components/MyTodo/my-todo.js";
+import TodoInput from "/js/components/TodoInput/todo-input.js";
+import TodoItem from "/js/components/TodoItem/todo-item.js";
 
 (async function(document) {
 
+	//let todoList = [];
+	const app = document.querySelector("#main");
+	const listPage = app.querySelector("#list-container");
 
-	CheckConnection();
+	checkConnectivity();
 	document.addEventListener('connexion-changed', ({detail}) => {
 		console.log(detail);
 	});
+
+	try{
+
+		/*const data = await fetch('http://localhost:3000/todo');
+		const json = await data.json();
+
+		const database = await openDB('app-store', 1, {
+			upgrade(db) {
+				db.createObjectStore('articles');
+			}
+		});
+
+		if (navigator.onLine) {
+			await database.put('articles', json, 'articles');
+		}
+
+		//document.addEventListener('todo-input', ({detail}) => {
+
+
+		let input = this.shadowRoot.querySelector("#add");
+		console.log(input);
+		input.addEventListener("submit", function(event){
+			json.push({text:document.querySelector("#add").value, checked:false});
+			database.put('articles', json, 'articles');
+			fetch('http://localhost:3000/todo', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({text:document.querySelector("#add").value, checked:false})
+			})
+		});
+		console.log(detail);
+	//});
+
+		const acticles = await database.get('articles', 'articles');
+
+		const todo = acticles.map(item => {
+			const todoElement = new MyTodo();
+
+			todoElement.initCard(item.text,
+				item.checked);
+
+			listPage.appendChild(todoElement);
+			return todoElement;
+		});*/
+
+	}catch(error) {
+		console.error(error);
+	}
 
 })(document);
--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,8 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -26,40 +27,192 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [
   {
-    "url": "images/manifest/icon-192x192.png",
-    "revision": "819372fe5e0b1955b4547943bd47cec4"
-  },
-  {
-    "url": "images/manifest/icon-512x512.png",
-    "revision": "be49c25d989daa06e2709e54d0dea4a3"
-  },
-  {
     "url": "index.html",
-    "revision": "4bec6a3b326d222940d7eb0f31609a74"
+    "revision": "3a7babaafe51c002ee19c87240704236"
   },
   {
     "url": "js/app.js",
-    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+    "revision": "0a6f5c617e9f8483adc035c6fddce525"
+  },
+  {
+    "url": "js/components/MyTodo/my-todo.css",
+    "revision": "c29f0b1e740b2ac6540a85cec37fb42d"
+  },
+  {
+    "url": "js/components/MyTodo/my-todo.js",
+    "revision": "3b4707cf77bf8e42bc89c0c8116f9785"
+  },
+  {
+    "url": "js/components/TodoInput/todo-input.js",
+    "revision": "72842d12f68c3020617770ca4b5c7b66"
+  },
+  {
+    "url": "js/components/TodoItem/todo-item.js",
+    "revision": "56242f0a02af687ba5a3d9bfacee5e5b"
+  },
+  {
+    "url": "js/connection.js",
+    "revision": "9f20b4b2ad61a65e35fb8fcd34e38893"
   },
   {
     "url": "manifest.json",
     "revision": "bccdfae42f0123760b3fcf3a170ae442"
   },
   {
-    "url": "package-lock.json",
-    "revision": "d44fb0f3ca034470dd760319dfd4807b"
+    "url": "styles/main.css",
+    "revision": "efdb1972fe1b63e0647b232a934b4803"
   },
   {
-    "url": "package.json",
-    "revision": "34df9ae5c6ab7417124b3d2046151702"
+    "url": "node_modules/idb/build/esm/index.js",
+    "revision": "1744c78524339333fc1a296a8d11baf9"
   },
   {
-    "url": "README.md",
-    "revision": "16771457651be0a4d3dcb64b736e55f3"
+    "url": "node_modules/idb/build/esm/chunk.js",
+    "revision": "c425e70eed99680fd3f14b1f70580f8a"
   },
   {
-    "url": "workbox-config.js",
-    "revision": "5af390134f84003b6f2eb631014e8470"
+    "url": "node_modules/lit-element/lib/css-tag.js",
+    "revision": "88a7776dc45a2f5f5a2a99b144d49075"
+  },
+  {
+    "url": "node_modules/lit-element/lib/decorators.js",
+    "revision": "aa875a7b763b574cfce40b684b733eb3"
+  },
+  {
+    "url": "node_modules/lit-element/lib/updating-element.js",
+    "revision": "2276cf333bd0a08bd8ae34783f36aa4c"
+  },
+  {
+    "url": "node_modules/lit-element/lit-element.js",
+    "revision": "72433c8e2d5d81583b7af68bad4822d6"
+  },
+  {
+    "url": "node_modules/lit-html/directives/async-append.js",
+    "revision": "20e3f940ebfcbf1fd1c3ff930140478c"
+  },
+  {
+    "url": "node_modules/lit-html/directives/async-replace.js",
+    "revision": "7732ef7434ee0095decce950f416bff8"
+  },
+  {
+    "url": "node_modules/lit-html/directives/cache.js",
+    "revision": "8aa0c9b2096899239e163a0727362c6f"
+  },
+  {
+    "url": "node_modules/lit-html/directives/class-map.js",
+    "revision": "13fd443f5b030395790886e21fa7ef6b"
+  },
+  {
+    "url": "node_modules/lit-html/directives/guard.js",
+    "revision": "ad09d68d2035c335d3be91e78a390ec7"
+  },
+  {
+    "url": "node_modules/lit-html/directives/if-defined.js",
+    "revision": "0b45cc46b88bc39118f14369e7f9141e"
+  },
+  {
+    "url": "node_modules/lit-html/directives/repeat.js",
+    "revision": "a3ed38e564c97093741ed7fe6ab53d1d"
+  },
+  {
+    "url": "node_modules/lit-html/directives/style-map.js",
+    "revision": "52f71857303971de342219f86e1be27c"
+  },
+  {
+    "url": "node_modules/lit-html/directives/unsafe-html.js",
+    "revision": "ff818bc767837badf6765e48c3903154"
+  },
+  {
+    "url": "node_modules/lit-html/directives/until.js",
+    "revision": "ee1eb6e410fa9920673890aece12b517"
+  },
+  {
+    "url": "node_modules/lit-html/lib/async-append.js",
+    "revision": "e72ae444630172678a2ce51dbf52beb6"
+  },
+  {
+    "url": "node_modules/lit-html/lib/async-replace.js",
+    "revision": "08d3c1a2322c52e2689758fb0bd6c912"
+  },
+  {
+    "url": "node_modules/lit-html/lib/default-template-processor.js",
+    "revision": "e96dc25c4a6ab961d383723b243bacb1"
+  },
+  {
+    "url": "node_modules/lit-html/lib/directive.js",
+    "revision": "1d0d4dd4117349ba63339aee90963f44"
+  },
+  {
+    "url": "node_modules/lit-html/lib/dom.js",
+    "revision": "45322140ad6ad2789e8ebeb95eec59f3"
+  },
+  {
+    "url": "node_modules/lit-html/lib/lit-extended.js",
+    "revision": "d24daec20065370d69db1eea3ca14252"
+  },
+  {
+    "url": "node_modules/lit-html/lib/modify-template.js",
+    "revision": "19cd46629fac6601423169f538f5b5ba"
+  },
+  {
+    "url": "node_modules/lit-html/lib/part.js",
+    "revision": "ffd11b35ced31f865d8381f3585892df"
+  },
+  {
+    "url": "node_modules/lit-html/lib/parts.js",
+    "revision": "ffdc54cd7f09acb476c53d96c99b0a99"
+  },
+  {
+    "url": "node_modules/lit-html/lib/render-options.js",
+    "revision": "b135ca8c29cdf7d1e3d0761b692e1663"
+  },
+  {
+    "url": "node_modules/lit-html/lib/render.js",
+    "revision": "e22a39942d0d752076163c996cf8fc56"
+  },
+  {
+    "url": "node_modules/lit-html/lib/repeat.js",
+    "revision": "8472ee5fdd935aa72259321a05f9471f"
+  },
+  {
+    "url": "node_modules/lit-html/lib/shady-render.js",
+    "revision": "6f7e199cd1cf7c2ae9bf6eba7a8c12c7"
+  },
+  {
+    "url": "node_modules/lit-html/lib/template-factory.js",
+    "revision": "dcdc52b73ba1f464ece3387ab1ce1b8d"
+  },
+  {
+    "url": "node_modules/lit-html/lib/template-instance.js",
+    "revision": "6c9495fce172762a92590b384bb4d3c3"
+  },
+  {
+    "url": "node_modules/lit-html/lib/template-processor.js",
+    "revision": "da7fb57119a10d43468d581552ceee39"
+  },
+  {
+    "url": "node_modules/lit-html/lib/template-result.js",
+    "revision": "692e28fbf475a700fcb68ebecfe37741"
+  },
+  {
+    "url": "node_modules/lit-html/lib/template.js",
+    "revision": "aa4ec6a7762d526e15f7c34b4e06c9c4"
+  },
+  {
+    "url": "node_modules/lit-html/lib/unsafe-html.js",
+    "revision": "68d76f033adbaf4fea06ad13cf0980ce"
+  },
+  {
+    "url": "node_modules/lit-html/lib/until.js",
+    "revision": "5c149fcee4354ebda36af2e9bb63a79a"
+  },
+  {
+    "url": "node_modules/lit-html/lit-html.js",
+    "revision": "b0f1b842794a5026a27829356f2a26ce"
+  },
+  {
+    "url": "node_modules/lit-html/polyfills/template_polyfill.js",
+    "revision": "0f0860f25b55c688dd6f1bd5685a824c"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
